72dedb9d17918ff82650d1c6f12ae68cf24b47b6
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-06-18 20:02:28 +0200

    👷(cli) Read file path

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
 use clap::Parser;
 
+#[derive(Parser, Debug)]
+struct Args {
+    #[clap(short, long, help="Lox source file path")]
+    file_path: String,
+}
+
+impl Args {
+    fn new() -> Result<Self, String> {
+        let args = Self::parse();
+        if args.file_path.is_empty() {
+            Err("No file path provided".to_string())
+        } else {
+            Ok(args)
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    Args::new().unwrap();
 }
